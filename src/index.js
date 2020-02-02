5450c5fff730b0e92d8460bb395bc07929bec8c0
Alex Shakun <alx.safari@gmail.com>
2020-02-02 16:10:45 +0300

    more linter stuff

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const greet = () => {
   const actual = readlineSync.question('May I have your name? ');
 
-  console.log(`Hello, ${actual}!`)
-}
+  console.log(`Hello, ${actual}!`);
+};
 
 export default greet;
