4b93ee9b79c965f1dcb1ecf76bd502d289d02ba7
Alex Shakun <alx.safari@gmail.com>
2020-02-02 15:57:55 +0300

    step 3 stuff

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import greet from '../src/index.js';
+
 
 console.log('Welcome to the Brain Games!');
+greet();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greet = () => {
+  const actual = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${actual}!`)
+}
+
+export default greet;
