86b31d6a44dfd6979f5c46d5bb5d2c63cc49861e
Alex Shakun <alx.safari@gmail.com>
2020-04-11 16:02:39 +0300

    step 9: brain prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runBrainPrimeGame from '../src/brain-prime/index.js';
+
+runBrainPrimeGame();
---
+++ src/brain-prime/index.js
@@ -0,0 +1,24 @@
+import { ask, createGameLauncher, getNumInRange } from '../index.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
+const gameLauncher = createGameLauncher(() => {
+  const numberToGuess = getNumInRange(1, 3571);
+  const correctAnswer = isPrime(numberToGuess) ? 'yes' : 'no';
+
+  const userAnswer = ask(
+    `Answer "yes" if given number is prime. Otherwise answer "no".\nQuestion: ${numberToGuess}`,
+  );
+
+  return [userAnswer, correctAnswer];
+});
+
+
+export default gameLauncher;
